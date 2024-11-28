b8deac53c5db922d8a4c5602b78ba1e5d47bc376
crohasang <hasang2691@naver.com>
2024-11-28 16:47:03 +0900

    feat(ProfileEllipse, RoundedBackGround, HomePage): ProfileEllipse, RoundedBackGround 컴포넌트 구현
    - ProfileEllipse 컴포넌트 생성
    - RoundedBackGround 컴포넌트 생성
    - HomePage에 해당 컴포넌트 적용

---
+++ src/components/homePage/ellipse/ProfileEllipse.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+const ProfileEllipse = styled.div`
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  background-color: var(--gray1, #D9D9D9);
+`;
+
+export default ProfileEllipse;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,49 @@
-export const HomePage = () => {
-  return (
-    <h1>HomePage</h1>
-  )
-}
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import RoundedBackground from '../components/homePage/RoundedBackground';
+import ProfileEllipse from '../components/homePage/ellipse/ProfileEllipse';
 
-export default HomePage
+const HomePage = () => {
+  return (
+    <div
+      css={css`
+        width: 100vw;
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        position: relative;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      `}
+    >
+      <div
+        css={css`
+          position: relative;
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        `}
+      >
+        <div
+          css={css`
+            position: absolute;
+            top: 38px;  
+            z-index: 2;
+          `}
+        >
+          <ProfileEllipse />
+        </div>
+        <RoundedBackground>
+          <div css={css`margin-top: 60px;`}>
+            <h1>HomePage</h1>
+          </div>
+        </RoundedBackground>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
