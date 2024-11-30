39ca467232af03285b020e571674e0cef907aece
chaeyoung103 <thdcodud103@naver.com>
2024-12-01 01:58:13 +0900

    fix: profile ellipse boder 추가

--- src/components/homePage/ellipse/ProfileEllipse.tsx
+++ src/components/homePage/ellipse/ProfileEllipse.tsx
@@ -4,6 +4,7 @@ const ProfileEllipse = styled.div<{ size?: string }>`
   width: ${({ size }) => size || '50px'};
   height: ${({ size }) => size || '50px'};
   border-radius: 50%;
+  border: 1px solid var(--gray1, #ba6c25);
   background-color: var(--gray1, #d9d9d9);
 `;
 
