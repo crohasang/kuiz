81057213aa2a2c663d784beb9af2d75577ac0253
GichuL <hanascreen@konkuk.ac.kr>
2024-12-01 13:58:40 +0900

    빈칸 채우기 퀴즈창 개발 시작

---
+++ src/pages/QuizPage/QuizScreen2.tsx
@@ -0,0 +1,23 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import {
+  TopBackground,
+  BottomBackground,
+  ButtonContainer,
+  Title,
+  Button,
+  ExplanationContainer,
+} from './quizscreen1_styles';
+import { Container } from '../../components/common/container/Container';
+import { useState, useEffect } from 'react';
+
+//더 나은 디자인에 관한 내용은.. 현재는 dummy data 넣어 놓음
+import option1 from '../../assets/correct_UI.png';
+import option2 from '../../assets/uncorrect_UI.png';
+import PrimaryButton from '../../components/common/button/PrimaryButton';
+
+
+//빈칸 채우기 퀴즈 유형(QuizScreen2)
+function QuizScreen2() {
+    
+}
